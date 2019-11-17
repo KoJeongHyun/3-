2e1ce467180000f6cfae2306d7ef288c0c3f0465
sjw8793 <49185019+sjw8793@users.noreply.github.com>
2019-11-17 13:16:13 +0900

    Update function.h

--- function.h
+++ function.h
@@ -1,9 +1,2 @@
 #pragma once
-void myFlush();
-void computerinput(int*);
-void transNum(char*, int*);
-int userinput(const char*, char*, int*);
-int checkDate(int*);
-int gamePlay(int*, int*, int*, int*);
-int ballChenk(int*, int*);
-int output(int, int*, int*);
+int checkData(int*);
