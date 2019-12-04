5d78b5e30b0de32f2b62b243b314448e90e5dff5
kokyeongbin <55739289+kokyeongbin@users.noreply.github.com>
2019-12-04 21:06:43 +0900

    코드 약간 수정해서 다시 다 올렸습니다

---
+++ checkData.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#define GAME_CHIPER 4
+
+int checkData(int *checkArr)
+{
+	int i, j;
+
+	for (i = 0; i < GAME_CHIPER - 1; ++i)
+		for (j = i + 1; j < GAME_CHIPER;++j)
+			if (checkArr[i] == checkArr[j])
+				return 1;
+	return 0;
+}
---
+++ checkData.h
@@ -0,0 +1,2 @@
+#pragma once
+int checkData(int*);
