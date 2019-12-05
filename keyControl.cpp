156544ce66face82d676ef4a6468ff8224b69dcc
JeongHyun <rhwjdgus112@naver.com>
2019-12-05 15:45:26 +0900

    교수님께 Pull Request

---
+++ keyControl.cpp
@@ -0,0 +1,27 @@
+#include<conio.h>
+#include"keyControl.h"
+
+int keyControl(void) {
+	char temp = _getch();
+
+	if (temp == 'w' || temp == 'W')
+	{
+		return UP;
+	}
+	else if (temp == 'a' || temp == 'A')
+	{
+		return LEFT;
+	}
+	else if (temp == 's' || temp == 'S')
+	{
+		return DOWN;
+	}
+	else if (temp == 'd' || temp == 'D')
+	{
+		return RIGHT;
+	}
+	else if (temp == ' ')
+	{ // 스페이스바(공백)이 선택 버튼  
+		return SUBMIT;
+	}
+}
